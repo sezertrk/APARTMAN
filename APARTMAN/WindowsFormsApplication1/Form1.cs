bb0e3963e1f0c4f85b83e82a84c855507f0e2db4
sezertrk <43032011+sezertrk@users.noreply.github.com>
2018-09-06 15:26:32 +0300

    Add files via upload

---
+++ APARTMAN/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int a = int.Parse(textBox1.Text);
+            int d = int.Parse(textBox2.Text);
+            int k = int.Parse(textBox3.Text);
+            int sabit = 20;
+            int sonuc = 0;
+            sonuc = a * d * k * sabit ;
+            MessageBox.Show(sonuc.ToString());
+        }
+    }
+}
